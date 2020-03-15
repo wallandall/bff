964b9b5acec5ef3d7ab2f0724e410d937073e90b
wallandall <wallandall@gmail.com>
2020-03-15 22:33:59 +0100

    Setup hompage and menu componet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import HomePage from "./pages/homepage/homepage.component";
+import "./App.css";
 
 function App() {
   return (
-    <div >
-    Hello
+    <div>
+      <HomePage />
     </div>
   );
 }

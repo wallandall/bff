d93a1edfe7392546e91b99309ebaa8f6a632eed1
wallandall <wallandall@gmail.com>
2020-03-24 20:18:40 +0100

    Added cart item dropdown and cart item

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import SHOP_DATA from "./shop.data.js";
+
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       collections: SHOP_DATA,
     };
@@ -15,8 +17,8 @@ class ShopPage extends React.Component {
     const { collections } = this.state;
     return (
       <div className="shop-page">
-        {collections.map(({ id, ...otherSectionProps }) => (
-          <CollectionPreview key="id" {...otherSectionProps} />
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
         ))}
       </div>
     );
